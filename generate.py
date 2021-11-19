84854d8f39f12e106c58a65718cb4ea6c3548ac0
Mingze Zheng <mzheng15@jh.edu>
2021-11-19 14:57:57 -0500

    Update generate.py
    Encapsulated into classes

--- generate.py
+++ generate.py
@@ -4,6 +4,57 @@ import time
 import copy
 
 
+class Seed(object):
+    def __init__(self, seed):
+        self.seed = seed
+    
+    def generate_board_origin(self):
+        """Generate a 9*9 sudoku
+
+        Returns:
+            list: A 9*9 list of integer which represents the original board
+        """
+        # generate a basic board fulfilled by '0'
+        random.seed = self.seed
+        self.sudo = np.zeros((9, 9), int)
+        num = random.randint(1, 9)
+        for row in range(9):
+            for col in range(9):
+                sudo_row = get_row(self.sudo, row)
+                sudo_col = get_col(self.sudo, col)
+                sudo_board = get_board(self.sudo, row, col)
+                while (num in sudo_row) or (num in sudo_col) or (num in sudo_board):
+                    num = num % 9 + 1
+                self.sudo[row, col] = num
+                num = num % 9 + 1
+        return self.sudo
+    
+    def generate_board_new(self, sudo):
+        """Generate a new sudoku board by exchanging row and column
+
+        Args:
+            sudo (list): A 9*9 list of integer
+
+        Returns:
+            list: A 9*9 list of integer which represents a new board
+        """
+        random.seed = self.seed
+        seed = self.seed
+        refresh = abs(seed) % 100
+        for i in range(refresh):
+            change_row = random.choice([0, 3, 6])
+            change_row_num = random.sample([0, 1, 2], k=2)
+            row_index_1 = change_row + change_row_num[0]
+            row_index_2 = change_row + change_row_num[1]
+            self.sudo[[row_index_1, row_index_2], :] = self.sudo[[row_index_2, row_index_1], :]
+
+            change_col = random.choice([0, 3, 6])
+            change_col_num = random.sample([0, 1, 2], k=2)
+            col_index_1 = change_col + change_col_num[0]
+            col_index_2 = change_col + change_col_num[1]
+            self.sudo[:, [col_index_1, col_index_2]] = self.sudo[:, [col_index_2, col_index_1]]
+        return self.sudo
+
 def get_row(sudo, row):
     """Get all the grids of the row where the grid is located
 
@@ -17,7 +68,6 @@ def get_row(sudo, row):
     return sudo[row, :]
 
 
-# get all the grids of the column where the grid is located
 def get_col(sudo, col):
     """Get all the grids of the column where the grid is located
 
@@ -31,7 +81,6 @@ def get_col(sudo, col):
     return sudo[:, col]
 
 
-# get all the grids of the small board where the grid is located
 def get_board(sudo, row, col):
     """Get all the grid of the small board where the grid is located
 
@@ -48,49 +97,6 @@ def get_board(sudo, row, col):
     return sudo[row_start: row_start+3, col_start: col_start+3]
 
 
-def generate_board_origin():
-    """Generate a 9*9 sudoku
-
-    Returns:
-        list: A 9*9 list of integer which represents the original board
-    """
-    # generate a basic board fulfilled by '0'
-    sudo = np.zeros((9, 9), int)
-    num = random.randint(1, 9)
-    for row in range(9):
-        for col in range(9):
-            sudo_row = get_row(sudo, row)
-            sudo_col = get_col(sudo, col)
-            sudo_board = get_board(sudo, row, col)
-            while (num in sudo_row) or (num in sudo_col) or (num in sudo_board):
-                num = num % 9 + 1
-            sudo[row, col] = num
-            num = num % 9 + 1
-    return sudo
-
-
-def generate_board_new(sudo):
-    """Generate a new sudoku board by exchanging row and column
-
-    Args:
-        sudo (list): A 9*9 list of integer
-
-    Returns:
-        list: A 9*9 list of integer which represents a new board
-    """
-    for i in range(100):
-        change_row = random.choice([0, 3, 6])
-        change_row_num = random.sample([0, 1, 2], k=2)
-        row_index_1 = change_row + change_row_num[0]
-        row_index_2 = change_row + change_row_num[1]
-        sudo[[row_index_1, row_index_2], :] = sudo[[row_index_2, row_index_1], :]
-
-        change_col = random.choice([0, 3, 6])
-        change_col_num = random.sample([0, 1, 2], k=2)
-        col_index_1 = change_col + change_col_num[0]
-        col_index_2 = change_col + change_col_num[1]
-        sudo[:, [col_index_1, col_index_2]] = sudo[:, [col_index_2, col_index_1]]
-    return sudo
 
 
 def generate_puzzle(sudo, level):
