a95f33eea46663be0a25cda9880d1169fdcc7105
Zixiu Liu <91161028+ZixiuLiu@users.noreply.github.com>
2021-11-19 23:14:40 -0500

    Update Users_UI.py
    Update RGB color into variables

--- Users_UI.py
+++ Users_UI.py
@@ -4,6 +4,7 @@ import pygame, copy
 
 
 def running():
+    background_color = (210, 210, 210)
     # initialize the pygame
     pygame.init()
     # create the screen (width, hight)
@@ -14,7 +15,7 @@ def running():
     icon = pygame.image.load('icon.png')
     pygame.display.set_icon(icon)
     # set background color
-    screen.fill((230, 230, 230))
+    screen.fill(background_color)
     # call function to set up 9*9 grid
     grid = [[0,9,0,0,3,5,0,8,7],
             [8,0,0,1,9,0,0,5,0],
@@ -43,7 +44,7 @@ def running():
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 position = pygame.mouse.get_pos()
-                update_grid = insert(screen, (position[0]//60, position[1]//60), text_font, grid, update_grid)
+                update_grid = insert(screen, (position[0]//60, position[1]//60), text_font, grid, update_grid, background_color)
             # check if the answer is correct
             if update_grid == solution:
                 pygame.quit()
@@ -53,25 +54,29 @@ def running():
                 return
 
 def grid_setup(screen, grid, text_font):
+    line_color = (0, 0, 0)
     for i in range(0, 10):
         if i % 3 == 0:
-            pygame.draw.line(screen, (0,0,0), (60*i + 60, 60), (60*i + 60, 600), 5)
-            pygame.draw.line(screen, (0,0,0), (60, 60*i + 60), (600, 60*i + 60), 5)
+            pygame.draw.line(screen, line_color, (60*i + 60, 60), (60*i + 60, 600), 5)
+            pygame.draw.line(screen, line_color, (60, 60*i + 60), (600, 60*i + 60), 5)
         else:
-            pygame.draw.line(screen, (0,0,0), (60*i + 60, 60), (60*i + 60, 600), 2)
-            pygame.draw.line(screen, (0,0,0), (60, 60*i + 60), (600, 60*i + 60), 2)
+            pygame.draw.line(screen, line_color, (60*i + 60, 60), (60*i + 60, 600), 2)
+            pygame.draw.line(screen, line_color, (60, 60*i + 60), (600, 60*i + 60), 2)
+    
     for i in range (0, len(grid)):
         for j in range (0, len(grid[i])):
             if 1 <= grid[i][j] <= 9:
                 value = text_font.render(str(grid[i][j]), True, (0,0,0))
                 screen.blit(value, ((j+1)*60 + 23, (i+1)*60 + 20))
 
-def insert(screen, position, text_font, grid, update_grid):
+def insert(screen, position, text_font, grid, update_grid, background_color):
+    shaded_color = (100, 100, 100)
+    text_insert_color = (0, 0, 200)
     while True:
         if 1 <= position[0] <= 9 and 1 <= position[1] <= 9 and grid[position[1] - 1][position[0] - 1] == 0:
             for event in pygame.event.get():
                 # # change the color into dark gray when selected
-                pygame.draw.rect(screen, (100, 100, 100), (position[0]*60 + 5, position[1]*60 + 5, 60 - 8, 60 - 8))
+                pygame.draw.rect(screen, shaded_color, (position[0]*60 + 5, position[1]*60 + 5, 60 - 8, 60 - 8))
                 pygame.display.update()
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -79,13 +84,13 @@ def insert(screen, position, text_font, grid, update_grid):
                 if event.type == pygame.KEYDOWN:
                     if event.key - 8 == 0: # checking with backspace (8 is the ascII number of 'backspace')
                         # replace the space with a rectangel with the same color as the background
-                        pygame.draw.rect(screen, (230, 230, 230), (position[0]*60 + 5, position[1]*60 + 5, 60 - 8, 60 - 8))
+                        pygame.draw.rect(screen, background_color, (position[0]*60 + 5, position[1]*60 + 5, 60 - 8, 60 - 8))
                         pygame.display.update()
                         update_grid[position[1] - 1][position[0] - 1] = event.key - 8
                         return update_grid
                     if 1 <= (event.key - 48) <= 9: # check input for 1-9
-                        pygame.draw.rect(screen, (230, 230, 230), (position[0]*60 + 5, position[1]*60 + 5, 60 - 8, 60 - 8))
-                        value = text_font.render(str(event.key - 48), True, (0, 0, 255))
+                        pygame.draw.rect(screen, background_color, (position[0]*60 + 5, position[1]*60 + 5, 60 - 8, 60 - 8))
+                        value = text_font.render(str(event.key - 48), True, text_insert_color)
                         screen.blit(value, (position[0]*60 + 23, position[1]*60 + 20))
                         pygame.display.update()
                         update_grid[position[1] - 1][position[0] - 1] = event.key - 48
