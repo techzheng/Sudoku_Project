e1b83c4c13c505cd9727f8213a70c48ec348df1b
Mingze Zheng <mzheng15@jh.edu>
2021-11-26 17:40:28 -0500

    Create decript.py
    A file to read and decrypt level data

---
+++ decript.py
@@ -0,0 +1,25 @@
+import os
+
+def decrypt(file_name, N=17947, D=10103):
+    messages = str()
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        message_list = []
+        for i in range(len(lines)):
+            column_list = lines[i].strip().split(" ")
+            message_list.append(column_list)
+        for i in range(len(message_list)):
+            for j in range(len(message_list[i])):
+                message_list[i][j] = int(message_list[i][j])
+    for i in range(0, len(message_list)):
+        messages = messages + ''.join([chr((s ** D) % N) for s in message_list[i]])
+        if i < len(message_list)-1:
+            messages = messages + '\n'
+    return messages
+
+if __name__ == '__main__':
+    ori_path = os.getcwd()
+    dat_path = ori_path + '\data\levels'
+    os.chdir(dat_path)
+    messages = decrypt('5-5.dat')
+    print(messages)
