63cce626009d8510535545c02c836ada222e27d4
Mingze Zheng <mzheng15@jh.edu>
2021-11-26 17:39:41 -0500

    Level_information_generate
    Add a script to automatically generate levels.

---
+++ encript.py
@@ -0,0 +1,42 @@
+import os
+import generate
+import re
+
+
+def encrypt(content, N=17947, E=7):
+    return [(ord(s) ** E) % N for s in str(content)]
+
+
+# def decrypt(enc_content, N=45236, D=20086):
+#     return ''.join([chr((s ** D) % N) for s in enc_content])
+
+
+def write_dat(file, content, N=17947, E=7):
+    filename = '.'.join(file.split('.')[0:-1]) + '.dat'
+    with open(filename, 'w+') as f:
+        enc_content = encrypt(content, N, E)
+        num_list = [str(x) for x in enc_content]
+        f.write(' '.join(num_list))
+        f.write('\n')
+
+
+if __name__ == '__main__':
+    L = (1, 2, 3, 4, 5)
+    ori_path = os.getcwd()
+    dat_path = ori_path + '\data\levels'
+    os.chdir(dat_path)
+    for i in L:
+        for j in L:
+            answer = generate.generate_board_new(
+                generate.generate_board_origin())
+            answer_out1 = re.sub(r"(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)",
+                                 r"\1,\2,\3,\4,\5,\6,\7,\8,\9", str(answer))
+            answer_out2 = re.sub(r'\s+', ',\n', answer_out1)
+            puzzle = generate.generate_puzzle(answer, i + (j-1)*0.2)
+            puzzle_out1 = re.sub(r"(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)",
+                                 r"\1,\2,\3,\4,\5,\6,\7,\8,\9", str(puzzle))
+            puzzle_out2 = re.sub(r'\s+', ',\n', puzzle_out1)
+            content = answer_out2 + ',\n' + puzzle_out2
+            ori_path = os.getcwd()
+            filename = str(i) + '-' + str(j) + '.dat'
+            write_dat(filename, content)
