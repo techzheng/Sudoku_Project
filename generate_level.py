6ef2b62136e4fc6289648765b36777c1f9be3997
Mingze Zheng <mzheng15@jh.edu>
2021-11-26 17:44:11 -0500

    Update generate_level.py
    Debug

--- generate_level.py
+++ generate_level.py
@@ -1,14 +1,10 @@
 import os
-import generate
+import generate_sudo
 import re
 
 
 def encrypt(content, N=17947, E=7):
     return [(ord(s) ** E) % N for s in str(content)]
-
-
-# def decrypt(enc_content, N=45236, D=20086):
-#     return ''.join([chr((s ** D) % N) for s in enc_content])
 
 
 def write_dat(file, content, N=17947, E=7):
@@ -27,12 +23,12 @@ if __name__ == '__main__':
     os.chdir(dat_path)
     for i in L:
         for j in L:
-            answer = generate.generate_board_new(
-                generate.generate_board_origin())
+            answer = generate_sudo.generate_board_new(
+                generate_sudo.generate_board_origin())
             answer_out1 = re.sub(r"(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)",
                                  r"\1,\2,\3,\4,\5,\6,\7,\8,\9", str(answer))
             answer_out2 = re.sub(r'\s+', ',\n', answer_out1)
-            puzzle = generate.generate_puzzle(answer, i + (j-1)*0.2)
+            puzzle = generate_sudo.generate_puzzle(answer, i + (j-1)*0.2)
             puzzle_out1 = re.sub(r"(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)\s(\d)",
                                  r"\1,\2,\3,\4,\5,\6,\7,\8,\9", str(puzzle))
             puzzle_out2 = re.sub(r'\s+', ',\n', puzzle_out1)
