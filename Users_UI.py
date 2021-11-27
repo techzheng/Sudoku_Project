3a4d48ae8f633fa7669dc0784878c622179c32e5
Zixiu Liu <91161028+ZixiuLiu@users.noreply.github.com>
2021-11-26 22:05:19 -0500

    Update Users_UI.py
    change location of variables.

--- Users_UI.py
+++ Users_UI.py
@@ -4,7 +4,7 @@ import pygame, copy, time, random
 import generate_sudo
 import read_level
 
-def home_window(score):
+def home_window(score = 100):
     background_color = (235, 235, 235)
     text_color = (0, 0, 0)
     line_color = (0, 0, 0)
@@ -373,4 +373,4 @@ def time_to_score(t_tot, score, diff):
     return 100
 
 if __name__ == "__main__":
-    home_window(score = 100)
+    home_window()
