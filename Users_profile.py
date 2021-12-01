1d461e96b8965c0702ee9dd695ed75c86c45099c
Mingze Zheng <mzheng15@jh.edu>
2021-12-01 16:00:49 -0500

    Create Users_profile.py
    Add a file to save users profile

---
+++ Users_profile.py
@@ -0,0 +1,44 @@
+import os
+data = {}
+
+def encrypt(content, N=17947, E=7):
+    """Encrypt level information
+
+    Args:
+        content (str): Level information
+        N (int, optional): A parameter for RSA encryption algorithm. Defaults to 17947.
+        E (int, optional): A parameter for RSA encryption algorithm. Defaults to 7.
+
+    Returns:
+        str: Encrypt message
+    """
+    return [(ord(s) ** E) % N for s in str(content)]
+
+
+def register(username):
+    data[username] = User(username)
+    filename = str(username) + '.dat'
+    print(filename)
+    with open(filename, 'w+') as f:
+        # enc_content = encrypt(data)
+        f.write(str(data))
+        f.write('\n')
+
+
+
+class User(object):
+    def __init__(self, username, init_score=100):
+        self.username = username
+        self.score = init_score
+
+    def user_info(self, score):
+        pass
+
+if __name__ == '__main__':
+    ori_path = os.getcwd()
+    dat_path = ori_path + '/data/users'
+    os.chdir(dat_path)
+
+    # print(dat_path)
+    # register('zmz')
+    print(str(data))
